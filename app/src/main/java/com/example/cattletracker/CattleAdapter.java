11a59744517595041f78507248fd928d6af32e43
glueck21 <tglueck19@gmail.com>
2021-04-19 20:18:11 -0500

    refactor changes from putting database in own folder

--- app/src/main/java/com/example/cattletracker/CattleAdapter.java
+++ app/src/main/java/com/example/cattletracker/CattleAdapter.java
@@ -8,7 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.cattletracker.DatabaseDescription.Cattle;
+import com.example.cattletracker.data.DatabaseDescription.Cattle;
 
 public class CattleAdapter
         extends RecyclerView.Adapter<CattleAdapter.ViewHolder> {
